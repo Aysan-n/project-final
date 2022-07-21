b4d527f4cf83a9dc4e72669b3b0ee5ab27876f7a
Aysan-n <103320860+Aysan-n@users.noreply.github.com>
2022-07-21 18:00:15 +0430

    Update Registration.py

--- NS-project/src/client/Registration.py
+++ NS-project/src/client/Registration.py
@@ -1,9 +1,9 @@
 import rsa
 
 
-def initiate_registration(messaging, pub_key, first_name, last_name, username, password):
+def initiate_registration(messaging, pub_key, first_name, last_name, username, password, key):
     cipher = rsa.encrypt(build_message(first_name, last_name, username, password).encode(), pub_key).hex()
-    messaging.send_message({'message_type': 'registration', 'cipher': cipher})
+    messaging.send_message({'message_type': 'registration', 'cipher': cipher, 'key':key})
     reply = messaging.receive()
     print(reply)
     if reply['status'] == 'failed':
