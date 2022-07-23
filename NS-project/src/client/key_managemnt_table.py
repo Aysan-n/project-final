d172f6a56d9dc05490bca7779b99816a6e28df47
Aysan-n <103320860+Aysan-n@users.noreply.github.com>
2022-07-23 19:03:22 +0430

    Update key_managemnt_table.py

--- NS-project/src/client/key_managemnt_table.py
+++ NS-project/src/client/key_managemnt_table.py
@@ -1,9 +1,10 @@
 import sqlite3
 
+
 def create():
     try:
-        connection=sqlite3.connect('key_management.db')
-        cursor=connection.cursor()
+        connection = sqlite3.connect('key_management.db')
+        cursor = connection.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS key_management_table
                       (file_name TEXT, enc_file_name TEXT, key TEXT, iv TEXT);''')
         connection.commit()
@@ -11,13 +12,13 @@ def create():
     except sqlite3.Error as error:
         print("Failed to connect sqliteDB", error)
 
-    
+
 def insert(file_name, enc_file_name, key, iv):
     try:
         connection = sqlite3.connect('key_management.db')
         cursor = connection.cursor()
         cursor.execute("INSERT INTO key_management_table VALUES ('" + file_name + "', '" + enc_file_name +
-                                    "', '" + key.hex() + "' , '" + iv.hex() + "');")
+                       "', '" + key.hex() + "' , '" + iv.hex() + "');")
         connection.commit()
         connection.close()
     except sqlite3.Error as error:
@@ -26,30 +27,41 @@ def insert(file_name, enc_file_name, key, iv):
 
 def find_key(file_name):
     try:
-        connection=sqlite3.connect('key_management.db')
-        cursor=connection.cursor()
-        sql_select_query ='''SELECT key,iv FROM key_management_table WHERE file_name=?'''
+        connection = sqlite3.connect('key_management.db')
+        cursor = connection.cursor()
+        sql_select_query = '''SELECT key,iv FROM key_management_table WHERE file_name=?'''
         cursor.execute(sql_select_query, (file_name,))
-        records=cursor.fetchall()
+        records = cursor.fetchall()
         cursor.close()
         connection.close()
         return records
     except sqlite3.Error as error:
-         print("Failed to read data from key_management_table)", error)
-def find_file(file_name):  
+        print("Failed to read data from key_management_table)", error)
+
+
+def find_file(file_name):
     try:
-        connection=sqlite3.connect('key_management.db')
-        cursor=connection.cursor()
-        sql_select_query ='''SELECT * FROM key_management_table WHERE file_name=?'''
+        connection = sqlite3.connect('key_management.db')
+        cursor = connection.cursor()
+        sql_select_query = '''SELECT * FROM key_management_table WHERE file_name=?'''
         cursor.execute(sql_select_query, (file_name,))
-        records=cursor.fetchall()
+        records = cursor.fetchall()
         cursor.close()
         connection.close()
         return records
     except sqlite3.Error as error:
-         print("Failed to read data from key_management_table)", error)  
+        print("Failed to read data from key_management_table)", error)
 
 
-
-
-
+def find_decrypted(enc_name):
+    try:
+        connection = sqlite3.connect('key_management.db')
+        cursor = connection.cursor()
+        sql_select_query = '''SELECT * FROM key_management_table WHERE enc_file_name=?'''
+        cursor.execute(sql_select_query, (enc_name,))
+        records = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return records
+    except sqlite3.Error as error:
+        print("Failed to read data from key_management_table)", error)
